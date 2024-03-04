bb7b1de2bd7c66a2c2bf05bf43db4b177d6429b9
down <tian_erjie1316@163.com>
2024-03-04 20:46:08 +0800

    day02
    nges to be committed:

--- code/myday01/client.cpp
+++ code/myday01/client.cpp
@@ -1,13 +1,15 @@
 #include<sys/socket.h>
-#include <arpa/inet.h>  //这个头文件包含了<netinet/in.h>，不用再次包含了
-#include <cstring>
-#include <cstdio>
+#include<arpa/inet.h>  //这个头文件包含了<netinet/in.h>，不用再次包含了
+#include<cstring>
+#include<cstdio>
+#include "util.h"
 int main(){
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errorif(sockfd == -1, "socket 创建错误");
     struct sockaddr_in serv_addr;
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8888);
-    connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));  
+    errorif(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket connect");
 }
---
+++ code/myday01/util.h
@@ -0,0 +1,4 @@
+#ifndef UTIL_H
+#define UTIL_H
+void errorif(bool condition, const char *errmsg);
+#endif
